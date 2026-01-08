47bfeaac093a6b184122898c1c7696241a733b5b
Dipanjan Maji <mdipanjan2002@gmail.com>
2026-01-08 18:47:39 +0530

    Delete src directory

--- src/laser_safety.cpp
+++
@@ -1,9 +0,0 @@
-#include "laser_safety.hpp"
-
-LaserSafety::LaserSafety() : Node("LaserSafety"){
-    std::cout << "Laser Safety node stared" << std::endl;
-}
-
-bool LaserSafety::connection(){
-    
-}
--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "safety_field/lidar_safety.hpp"
-
-int main(int argc, char * argv[]){
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<LidarSafety>());
-    rclcpp::shutdown();
-    return 0;
-}
